b913ac10c45b5fb0ff520f968b30738413e3d99c
oscarsalsa <oscarsal0223@gmail.com>
2024-08-16 17:00:43 -0700

    Backend updates

---
+++ app.js
@@ -0,0 +1,11 @@
+//EXPRESS
+const express = require("express");
+const app = express();
+const userRouter = require('./routes/userRoutes')
+
+app.use(express.json());
+
+//ROUTES
+app.use('/api/v1/users', userRouter)
+
+module.exports = app;
